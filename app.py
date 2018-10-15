88af81b878d5766fa08cc66f4485a3520139478b
samezarus <samezarus@gmail.com>
2018-10-15 16:30:35 +1000

    комментарий коммита

--- app.py
+++ app.py
@@ -17,44 +17,38 @@ def createDayFolder(log_folder):
 		os.makedirs(day_folder)
 	return day_folder
 
+def createDayFile(log_folder):
+	day_file = log_folder+str(datetime.datetime.now().date()) + '.log'
+	return day_file
+
 # --------------------------------------------------------------------------------------------
 
-port     = 2300
-host     = "172.16.17.2"
-user     = "smdr"
-password = "pccsmdr"
+port = 2300
+host = '172.16.17.2'
+user = 'smdr'
+password = 'pccsmdr'
 
-log_folder = "/var/log/ats_loger/"
+log_folder = '/var/log/ats_loger/'
 
 telnet = telnetlib.Telnet()
 telnet.open(host, port)
 
-telnet.read_until("-".encode('ascii'))
-telnet.write(user.encode('ascii') + b"\r")
+telnet.read_until('-'.encode('ascii'))
+telnet.write(user.encode('ascii') + b'\r')
 
-telnet.read_until("Enter Password:".encode('ascii'))
+telnet.read_until('Enter Password:'.encode('ascii'))
 telnet.write(password.encode('ascii') + b"\r")
 
-day_folder_path = createDayFolder(log_folder) +"/log.txt"
+#day_folder_path = createDayFolder(log_folder) +'/log.txt'
+day_file_name = createDayFile(log_folder)
 
 while True:
 	time.sleep(1)
 
 	s = str(telnet.read_very_eager())
-	if (s != "" and s != "b''"):
-		day_folder = open(day_folder_path, "+a")
+	if (s != '' and s != "b''"):
+		day_folder = open(day_file_name, '+a')
 		day_folder.write(s)
 		day_folder.close()
-		print(s)
-	s = ""
-
-
-
-
-
-
-
-
-
-
-
+		#print(s)
+	s = ''
